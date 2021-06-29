9645c28c33423b3898c5bac703513183803eca93
Matias Moya <mat.moyad@duocuc.cl>
2021-06-29 15:59:33 -0400

    a

--- principal/models.py
+++ principal/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+class Noticia (models.Model):
+    id_noticia=models.IntegerField(primary_key=True)
+    titulo=models.CharField(max_length=100)
+    descripcion=models.CharField(max_length=300)
+    imagen=models.CharField(max_length=100, default='')
