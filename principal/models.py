dc8a2c1f6d35b2c3db2f42dd59328d9db6599418
Matias Moya <mat.moyad@duocuc.cl>
2021-06-29 16:46:09 -0400

    a

--- principal/models.py
+++ principal/models.py
@@ -8,3 +8,11 @@ class Noticia (models.Model):
     titulo=models.CharField(max_length=100)
     descripcion=models.CharField(max_length=300)
     imagen=models.CharField(max_length=100, default='')
+    categoria=models.ForeignKey(categoria,on_delete=models.CASCADE)
+
+
+
+class categoria (models.Model):
+    id_categoria=models.IntegerField(primary_key=True)
+    nombre=models.CharField(max_length=100)
+    
