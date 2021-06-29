5eaa833e0c48b2041a3b14c15f1ed27f629eb629
NicolRiveros <nicolcriverosp@gmail.com>
2021-06-29 18:38:20 -0400

    tablas creadas

--- principal/models.py
+++ principal/models.py
@@ -3,16 +3,13 @@ from django.db.models.fields import CharField
 
 # Create your models here.
 
-class Noticia (models.Model):
-    id_noticia=models.IntegerField(primary_key=True)
+class Categoria(models.Model):
+    idCategoria=models.IntegerField(primary_key=True)
+    nombreCategoria=models.CharField(max_length=100)
+
+class Noticia(models.Model):
+    idNoticia=models.IntegerField(primary_key=True)
     titulo=models.CharField(max_length=100)
     descripcion=models.CharField(max_length=300)
-    imagen=models.CharField(max_length=100, default='')
-    categoria=models.ForeignKey(categoria,on_delete=models.CASCADE)
-
-
-
-class categoria (models.Model):
-    id_categoria=models.IntegerField(primary_key=True)
-    nombre=models.CharField(max_length=100)
+    categoria=models.ForeignKey(Categoria, on_delete=models.CASCADE)
     
